94f8d2066eaae588ac35e5013a5f5afc9f871949
Chris Cosby <chris.cosby@aptplatforms.com>
2018-04-02 17:17:54 -0400

    Add MongoDB auth options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,36 @@
 
 mkdir -p /usr/lib/unifi/data
 
-envsubst >>/usr/lib/unifi/data/system.properties <<_EOT_
+grep '^uuid=' /usr/lib/unifi/data/system.properties >/system.properties
+
+if [ "$MONGO_REMOTE" = "true" ]
+then
+    envsubst >>/system.properties <<_EOT_
 db.mongo.local=false
-db.mongo.uri=mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}
-statdb.mongo.uri=mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}_stat
 unifi.db.name=${MONGO_DB}
 _EOT_
 
+    if [ "$MONGO_AUTH" = "true" ]
+    then
+        envsubst >>/system.properties <<_EOT_
+db.mongo.uri=mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}
+statdb.mongo.uri=mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}_stat
+_EOT_
+
+    else
+        envsubst >>/system.properties <<_EOT_
+db.mongo.uri=mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}
+statdb.mongo.uri=mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}_stat
+_EOT_
+    fi
+
+else
+    cat >>/system.properties <<_EOT_
+unifi.db.extraargs=--smallfiles
+_EOT_
+
+fi
+
+: ${JAVA_Xmx:='256M'}
+cat /system.properties >/usr/lib/unifi/data/system.properties
 java -Xmx${JAVA_Xmx} -jar /usr/lib/unifi/lib/ace.jar start
